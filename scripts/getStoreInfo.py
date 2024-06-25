19b1724ec9e329856b512fa76617f868571f16fd
Morita-Daiki <tamo.d.tamo@gmail.com>
2024-06-26 01:54:28 +0900

    add progress bar

--- scripts/getStoreInfo.py
+++ scripts/getStoreInfo.py
@@ -1,5 +1,6 @@
 import sqlite3
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 # Connect to the SQLite database (creates a new database if it does not exist)
 conn = sqlite3.connect('database/store_data.db')
@@ -9,10 +10,12 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS stores
                 (store_name TEXT, prefecture_name TEXT, full_address TEXT, phone_number TEXT)''')
 
-for step_num in range(1, 72):  # Loop through steps 1 to 71
-    print(f"step is {step_num}")
-    file_path = f'html_data/step_{step_num}.html'
+progress = tqdm(total=72, desc="Processing", unit="step")
 
+for step_num in range(1, 72):  # Loop through steps 1 to 71
+    progress.update()
+
+    file_path = f'html_data/step_{step_num}.html'
     try:
         with open(file_path, 'r') as file:
             html_content = file.read()
@@ -27,14 +30,20 @@ for step_num in range(1, 72):  # Loop through steps 1 to 71
             full_address = ''.join(address.split()[1:])
             phone_number = item.find('dl', class_='result-item_tel').find('a').text.strip()
 
-            # Insert store information into the database
-            cursor.execute('INSERT INTO stores VALUES (?, ?, ?, ?)',
-                           (store_name, prefecture_name, full_address, phone_number))
+            # Check if the store with the same name and phone number already exists
+            cursor.execute('SELECT * FROM stores WHERE store_name=? AND phone_number=?',
+                           (store_name, phone_number))
+            existing_record = cursor.fetchone()
 
-        conn.commit()
+            if existing_record is None:  # If no match found, insert the store information
+                cursor.execute('INSERT INTO stores VALUES (?, ?, ?, ?)',
+                               (store_name, prefecture_name, full_address, phone_number))
+                conn.commit()
 
     except FileNotFoundError:
         print(f"File {file_path} not found. Skipping...")
 
 # Close the database connection
 conn.close()
+progress.update()
+progress.close()
