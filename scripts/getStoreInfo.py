bcdbce9fce395b1a4b34c9dcd2f40ac1f90cedc6
Morita-Daiki <tamo.d.tamo@gmail.com>
2024-06-29 14:26:19 +0900

    add store_type

--- scripts/getStoreInfo.py
+++ scripts/getStoreInfo.py
@@ -8,11 +8,11 @@ cursor = conn.cursor()
 
 # Create a table to store the store information
 cursor.execute('''CREATE TABLE IF NOT EXISTS stores
-                (store_name TEXT, prefecture_name TEXT, full_address TEXT, phone_number TEXT)''')
+                (store_name TEXT, store_type TEXT, prefecture_name TEXT, full_address TEXT, phone_number TEXT)''')
 
 progress = tqdm(total=111, desc="Processing", unit="step")
 
-for step_num in range(1, 111):  # Loop through steps 1 to 110
+for step_num in range(1, 111):  # Loop through steps 1 to 111
     progress.update()
 
     file_path = f'html_data/step_{step_num}.html'
@@ -25,19 +25,20 @@ for step_num in range(1, 111):  # Loop through steps 1 to 110
 
         for item in result_items:
             store_name = item.find('span').text.strip()
+            store_type, store_name = store_name.split(' ', 1)
             address = item.find('p', class_='result-item_address').text.strip()
             prefecture_name = address.split()[0]
-            full_address = ''.join(address.split()[1:])
+            full_address = ' '.join(address.split()[1:])
             phone_number = item.find('dl', class_='result-item_tel').find('a').text.strip()
 
             # Check if the store with the same name and phone number already exists
-            cursor.execute('SELECT * FROM stores WHERE store_name=? AND phone_number=?',
-                           (store_name, phone_number))
+            cursor.execute('SELECT * FROM stores WHERE store_name=? AND phone_number=? AND store_type=?',
+                           (store_name, phone_number,store_type))
             existing_record = cursor.fetchone()
 
             if existing_record is None:  # If no match found, insert the store information
-                cursor.execute('INSERT INTO stores VALUES (?, ?, ?, ?)',
-                               (store_name, prefecture_name, full_address, phone_number))
+                cursor.execute('INSERT INTO stores VALUES (?, ?, ?, ?, ?)',
+                               (store_name, store_type, prefecture_name, full_address, phone_number))
                 conn.commit()
 
     except FileNotFoundError:
