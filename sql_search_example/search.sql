6e8802aadef7a31578ed0b913e747dd56bfcee16
Morita-Daiki <tamo.d.tamo@gmail.com>
2024-07-13 00:15:49 +0900

    add sql search example

---
+++ sql_search_example/search.sql
@@ -0,0 +1,6 @@
+SELECT store_name
+FROM stores
+WHERE prefecture_name = '岐阜県'
+GROUP BY store_name
+HAVING COUNT(DISTINCT store_type) = 2
+AND SUM(CASE WHEN store_type NOT IN ('しまむら', 'アベイル') THEN 1 ELSE 0 END) = 0;
